a221c6c5ccc20bc939c66083abd46033f78c2ecb
Sullivan <andisonsully@hotmail.com>
2026-01-16 14:45:48 -0800

    Removing all audio features for now as all methods to play audio seemed to have failed on actual device (although working in browsers)

--- 3DSLibrary/services/main.js
+++ 3DSLibrary/services/main.js
@@ -1,29 +1,17 @@
-var x = document.getElementById("backgroundMusic");
-
-function playAudio() {
-	x.play();
-}
-
-function pauseAudio() {
-	x.pause();
-}
 
 /* Special cases for button pressing */
-var control = function(ev) {
-	switch(ev.which) {
-		case 13: alert("A"); break;
-		case 37: pauseAudio(); break;
-		case 38: alert("up"); break;
-		case 39: playAudio(); break;
-		case 40: alert("down"); break;
-	}
-};
+// var control = function(ev) {
+// 	switch(ev.which) {
+// 		case 13: alert("A"); break;
+// 		case 37: alert("left"); break;
+// 		case 38: alert("up"); break;
+// 		case 39: alert("right"); break;
+// 		case 40: alert("down"); break;
+// 	}
+// };
 
 
 (function(){
-
-	document.addEventListener('keydown', control);
-
 	/*This function continuously scrolls the screen to the 40,215 coordinates */
 	var center = function(){
 		/* Scroll to designated coordinates */
@@ -33,7 +21,6 @@ var control = function(ev) {
 
 	/* When content is loaded */
 	document.addEventListener('DOMContentLoaded', function(ev) {
-		playAudio();
 		/* Call center function every milisecond*/
 		setInterval(center, 1);
 	}, false);
