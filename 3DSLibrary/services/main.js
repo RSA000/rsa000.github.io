c62345878621b07fb6bf48294698b13b692c13ed
Sullivan <andisonsully@hotmail.com>
2026-01-16 19:09:37 -0800

    Added main.js functionality to update header and subtitle when a link is selecetd by the keyboard (tab) or 3DS D-pad

--- 3DSLibrary/services/main.js
+++ 3DSLibrary/services/main.js
@@ -1,27 +1,57 @@
 
-/* Special cases for button pressing */
-// var control = function(ev) {
-// 	switch(ev.which) {
-// 		case 13: alert("A"); break;
-// 		case 37: alert("left"); break;
-// 		case 38: alert("up"); break;
-// 		case 39: alert("right"); break;
-// 		case 40: alert("down"); break;
-// 	}
-// };
-
-
+/*
+* This is an immediately invoked function expression.
+* This means that after this function is created it will immediately run.
+* All variables are within the functions scope and not the main programs global scope;
+* this is a means of encapsulating data.
+*/
 (function(){
-	/*This function continuously scrolls the screen to the 40,215 coordinates */
+
+
+	/* The changeHeader function changes the top screen's header text. */
+	var changeHeader = function(newHeading){
+		document.getElementById("topHeading").innerHTML = newHeading;
+	};
+
+	/* Change substitle paragraph under heading of top screen. */
+	var changeSubtitle = function(newSubtitle){
+			document.getElementById("topSubtitle").innerHTML = newSubtitle;
+	};
+
+
+	/* This initiateLinks function add necessary listeners to each link on the page */
+	var initiateLinks = function(){
+		// Save all <a> tags in variable "links"
+		var links = document.querySelectorAll('a');
+
+		// For each <a> tag element in links:
+		links.forEach(function(link) {
+			// When selected with keyboard (tab or 3DS d-pad):
+			link.addEventListener('focus', function(e) {
+				// Change header based on link text
+				changeHeader(this.innerText);
+				changeSubtitle(this.getAttribute('description'));
+			});
+		});
+	}
+
+
+	/* The center function scrolls the screen to the 40,215 coordinates */
 	var center = function(){
-		/* Scroll to designated coordinates */
+		// Scroll to designated coordinates
 		window.scrollTo(40,215);
 	};
 
 
+
+
 	/* When content is loaded */
 	document.addEventListener('DOMContentLoaded', function(ev) {
-		/* Call center function every milisecond*/
+
+		// Add event listeners to links
+		initiateLinks();
+
+		// Call center function every milisecond
 		setInterval(center, 1);
 	}, false);
 })()
