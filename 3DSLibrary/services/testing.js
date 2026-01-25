412a4ee9dc56f38030486017b9b93f85b1525dd5
Sullivan <andisonsully@hotmail.com>
2026-01-25 15:34:36 -0800

    updated testing.js, fixed css

--- 3DSLibrary/services/testing.js
+++ 3DSLibrary/services/testing.js
@@ -1,17 +1,8 @@
 (function(){
 
-var gamepad = navigator.webkitGetGamepads;
+alert(navigator.webkitGetGamepads.axes);
+alert(navigator.webkitGetGamepads.buttons);
 
-
-window.addEventListener("gamepadconnected", (e) => {
-    alert(
-        "Gamepad connected at index %d: %s. %d buttons, %d axes.",
-        e.gamepad.index,
-        e.gamepad.id,
-        e.gamepad.buttons.length,
-        e.gamepad.axes.length,
-    );
-}, false);
 
 document.addEventListener("keydown", function(event) {
     alert("Keycode: " + event.keyCode);
