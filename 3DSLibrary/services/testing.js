8d8defbf1b64ea6e30fc389448739021c8f347b9
Sullivan <andisonsully@hotmail.com>
2026-01-25 15:45:52 -0800

    updated testing.js

--- 3DSLibrary/services/testing.js
+++ 3DSLibrary/services/testing.js
@@ -1,24 +1,25 @@
 (function(){
 
     // Get the array of gamepads
-    var gamepads = navigator.webkitGetGamepads();
+    var gamepads = navigator.webkitGetGamepads;
 
     if (gamepads && gamepads.length > 0 && gamepads[0]) {
         var gp = gamepads[0];
 
+        var log = "";
         // Access axes
         var axes = gp.axes;
-        alert("Circle pad X: " + axes[0]);
-        alert("Circle pad Y: " + axes[1]);
-        alert("C-stick X: " + axes[2]);
-        alert("C-stick Y: " + axes[3]);
+        log += (" Circle pad X: " + axes[0]) + "\n";
+        log += (" Circle pad Y: " + axes[1]) + "\n";
+        log += (" C-stick X: " + axes[2]) + "\n";
+        log += (" C-stick Y: " + axes[3]) + "\n";
 
         // Access buttons
         var buttons = gp.buttons;
-        alert("Button A (index 1): " + buttons[1]);
-        alert("Button B (index 0): " + buttons[0]);
-        alert("Up (index 12): " + buttons[12]);
-        alert("Down (index 13): " + buttons[13]);
+        log += ("Button A (index 1): " + buttons[1]);
+        log += ("Button B (index 0): " + buttons[0]);
+        log += ("Up (index 12): " + buttons[12]);
+        log +=("Down (index 13): " + buttons[13]);
     } else {
         alert("No gamepad connected");
     }
