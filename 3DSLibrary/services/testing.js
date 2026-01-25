7bb942572217465830f2e43cd04dc6e2ff916496
Sullivan <andisonsully@hotmail.com>
2026-01-25 15:37:08 -0800

    updated testing.js

--- 3DSLibrary/services/testing.js
+++ 3DSLibrary/services/testing.js
@@ -1,8 +1,14 @@
 (function(){
 
-alert(navigator.webkitGetGamepads.axes);
-alert(navigator.webkitGetGamepads.buttons);
+    // Get the array of gamepads
+    var gamepads = navigator.getGamepads ? navigator.getGamepads() : [];
 
+    if (gamepads.length > 0 && gamepads[0]) {
+        alert("Axes: " + gamepads[0].axes);
+        alert("Buttons: " + gamepads[0].buttons);
+    } else {
+        alert("No gamepads connected");
+    }
 
 document.addEventListener("keydown", function(event) {
     alert("Keycode: " + event.keyCode);
