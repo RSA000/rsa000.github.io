6a3981b16c7ed25de847f0de84ed77410c6b0b80
Sullivan <andisonsully@hotmail.com>
2026-01-25 15:59:41 -0800

    updated testing.js logging gampepad

--- 3DSLibrary/services/testing.js
+++ 3DSLibrary/services/testing.js
@@ -1,30 +1,34 @@
 (function(){
 
-    // Get the array of gamepads
-    var gamepads = navigator.webkitGetGamepads();
+    function logGamepad(){
+        // Get the array of gamepads
+        var gamepads = navigator.webkitGetGamepads();
 
-    if (gamepads && gamepads.length > 0 && gamepads[0]) {
-        var gp = gamepads[0];
+        if (gamepads && gamepads.length > 0 && gamepads[0]) {
+            var gp = gamepads[0];
 
-        var log = "";
-        // Access axes
-        var axes = gp.axes;
-        log += (" Circle pad X: " + axes[0]) + "\n";
-        log += (" Circle pad Y: " + axes[1]) + "\n";
-        log += (" C-stick X: " + axes[2]) + "\n";
-        log += (" C-stick Y: " + axes[3]) + "\n";
+            var log = "";
+            // Access axes
+            var axes = gp.axes;
+            log += (" Circle pad X: " + axes[0]) + "\n";
+            log += (" Circle pad Y: " + axes[1]) + "\n";
+            log += (" C-stick X: " + axes[2]) + "\n";
+            log += (" C-stick Y: " + axes[3]) + "\n";
 
-        // Access buttons
-        var buttons = gp.buttons;
-        log += ("Button A (index 1): " + buttons[1]);
-        log += ("Button B (index 0): " + buttons[0]);
-        log += ("Up (index 12): " + buttons[12]);
-        log +=("Down (index 13): " + buttons[13]);
-        alert(log);
-        document.getElementById("topSubtitle").innerHTML = log;
-    } else {
-        alert("No gamepad connected");
+            // Access buttons
+            var buttons = gp.buttons;
+            log += ("Button A (index 1): " + buttons[1]);
+            log += ("Button B (index 0): " + buttons[0]);
+            log += ("Up (index 12): " + buttons[12]);
+            log +=("Down (index 13): " + buttons[13]);
+            alert(log);
+            document.getElementById("topSubtitle").innerText = log;
+        } else {
+            alert("No gamepad connected");
+        }
+
     }
+
 
 document.addEventListener("keydown", function(event) {
     alert("Keycode: " + event.keyCode);
