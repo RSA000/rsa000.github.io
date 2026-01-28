fb4350b70c1d221cc07ab15af1b46c312d71cdb2
Sullivan <andisonsully@hotmail.com>
2026-01-28 14:48:59 -0800

    enbettering the CSS and center logic

--- 3DSLibrary/services/common.js
+++ 3DSLibrary/services/common.js
@@ -9,8 +9,8 @@ const BACKSPACE = 8;
 const F5 = 116;
 const ENTER = 13;
 // Declare constant value for centering screen.
-const centerX = 148;
-const centerY = 280;
+const centerX = 152;
+const centerY = 270;
 
 
 /**
