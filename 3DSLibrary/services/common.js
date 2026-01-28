d42895355fc3861882f7e9e3a4ce6658f820c363
Sullivan <andisonsully@hotmail.com>
2026-01-28 13:59:22 -0800

    centering tweaking again

--- 3DSLibrary/services/common.js
+++ 3DSLibrary/services/common.js
@@ -9,8 +9,8 @@ const BACKSPACE = 8;
 const F5 = 116;
 const ENTER = 13;
 // Declare constant value for centering screen.
-const centerX = 150;
-const centerY = 350;
+const centerX = 155;
+const centerY = 250;
 
 
 /**
