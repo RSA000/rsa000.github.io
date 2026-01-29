d2309f91e68c25899e95d4a9fbb63a5883d717d8
Sullivan <andisonsully@hotmail.com>
2026-01-28 23:14:09 -0800

    updated

--- 3DSLibrary/services/common.js
+++ 3DSLibrary/services/common.js
@@ -9,8 +9,8 @@ const BACKSPACE = 8;
 const F5 = 116;
 const ENTER = 13;
 // Declare constant value for centering screen.
-const centerX = 175;
-const centerY = 250;
+const centerX = 140;
+const centerY = 285;
 
 
 /**
