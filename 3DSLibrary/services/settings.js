c50b2575a10924ddd47339b04a939ec7d39b07d7
Sullivan <andisonsully@hotmail.com>
2026-01-31 15:07:03 -0800

    fixing settings buttons

--- 3DSLibrary/services/settings.js
+++ 3DSLibrary/services/settings.js
@@ -8,8 +8,8 @@
  */
 (function(){
 
-    const anchors = document.getElementsByTagName("a");
-    const anchorLength = anchors.length;
+    const buttonss = document.getElementsByTagName("buttons");
+    const buttonssLength = buttons.length;
     var index = 0;
     var topHeading = document.getElementsByClassName("topHeading")[0];
     var topSubtitle = document.getElementsByClassName("topSubtitle")[0];
@@ -43,42 +43,42 @@
      * Process keydown logic. Call this when using window.onkeydown, and you want to use the global.js input detection system
      * @param {KeyboardEvent} event
      */
-    // function menuHandleKeyDown(event, element){
-    //     // Prevent default action when key is pressed down.
-    //     preventKey(event);
-    //     // Switch case for each button press code.
-    //     switch(event.keyCode){
-    //          // Case for up button.
-    //          case UP:
-    //              // If index is above 0.
-    //              if (index > 0){
-    //                  // Update index -1.
-    //                  index -= 1;
-    //              }
-    //              // If index is 0.
-    //              else{
-    //                  // Set index to last anchor.
-    //                  index = anchorLength - 1
-    //              }
-    //              // Focus on current index.
-    //              anchors[index].focus();
-    //              break;
-    //              // Case for down button.
-    //          case DOWN:
-    //              // If index is not above anchorLength.
-    //              if (index < anchorLength -1){
-    //                  // Increment index.
-    //                  index += 1;
-    //              }
-    //              // If index is at last anchor.
-    //              else{
-    //                  index = 0;
-    //              }
-    //              // Focus on current anchor index.
-    //              anchors[index].focus();
-    //              break;
-    //      }
-    // };
+     function menuHandleKeyDown(event, element){
+         // Prevent default action when key is pressed down.
+         preventKey(event);
+         // Switch case for each buttons press code.
+         switch(event.keyCode){
+              // Case for up buttons.
+              case UP:
+                  // If index is above 0.
+                  if (index > 0){
+                      // Update index -1.
+                      index -= 1;
+                  }
+                  // If index is 0.
+                  else{
+                      // Set index to last anchor.
+                      index = buttonssLength - 1
+                  }
+              // Focus on current index.
+                  buttons[index].focus();
+                  break;
+                  // Case for down buttons.
+              case DOWN:
+                  // If index is not above buttonssLength.
+                  if (index < buttonssLength -1){
+                      // Increment index.
+                      index += 1;
+                  }
+                  // If index is at last anchor.
+                  else{
+                      index = 0;
+                  }
+                  // Focus on current anchor index.
+                  buttons[index].focus();
+                  break;
+          }
+     }
 
 
 
@@ -94,18 +94,18 @@
             menuHandleKeyDown(e, document.getElementById('themeSettings'));
         });
 
-        // Store all <a> tags within the "lowerScreenContents" div in variable "anchors."
-        var buttons = document.querySelectorAll("#themeSettings button");
+        // Store all <a> tags within the "lowerScreenContents" div in variable "buttons."
+        var buttonss = document.querySelectorAll("#themeSettings buttons");
         // For each anchor, add event listener.
-        for(var i = 0, l = buttons.length; i<l; i++){
+        for(var i = 0, l = buttonss.length; i<l; i++){
             // For each anchor, add event listener.
 
-            buttons[i].setAttribute('tabindex', i);
+            buttonss[i].setAttribute('tabindex', i);
             // When focused on, apply active function with "this" selected anchor.
-            buttons[i].addEventListener('focus', active, false);
-            // When no anchors are selected, revert to greeting heading and subtitle.
-            buttons[i].addEventListener('blur', inactive, false);
-            buttons[i].addEventListener("click", click, false);
+            buttonss[i].addEventListener('focus', active, false);
+            // When no buttons are selected, revert to greeting heading and subtitle.
+            buttonss[i].addEventListener('blur', inactive, false);
+            buttonss[i].addEventListener("click", click, false);
         }
     }, false);
 
