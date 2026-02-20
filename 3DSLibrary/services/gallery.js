c7733fc9960b6d4441e9e729f58abb384fe07e78
Sullivan <andisonsully@hotmail.com>
2026-02-20 14:30:29 -0800

    updated gallery.js

--- 3DSLibrary/services/gallery.js
+++ 3DSLibrary/services/gallery.js
@@ -60,7 +60,7 @@
     /* When content is loaded. */
     document.addEventListener('DOMContentLoaded', function(ev) {
 
-        window.removeEventListener("keydown", handleKeyDown);
+        window.removeEventListener("keydown", menuHandleKeyDown);
 
         // Add event listener for when a key is pressed down.
         window.addEventListener("keydown", function(e) {
