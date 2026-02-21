c1514505bda69714ad2e50167c14aec7633e107a
Sullivan <andisonsully@hotmail.com>
2026-02-20 22:16:04 -0800

    updated common.js

--- 3DSLibrary/services/common.js
+++ 3DSLibrary/services/common.js
@@ -122,14 +122,14 @@ function configDesktop(){
         "bottom": "65%",
     });
     $("#indexScreenUpperRightPhoto").css({
-        "height": "128px";
-        "width": "116px";
-        "left": "500px";
+        "height": "128px",
+        "width": "116px",
+        "left": "500px",
     });
     $("#indexScreenUpperLeftPhoto").css({
-        "height": "128px";
-        "width": "116px";
-        "left": "500px";
+        "height": "128px",
+        "width": "116px",
+        "left": "500px",
     });
     $(".lowerScreen").css({
         "width": "750px"
